42c3f755026beb5c32c521ee67a77d1f2dc739c5
MAYANK RAJ <mayank1312.raj@gmail.com>
2026-07-08 16:53:21 +0530

    Time: 3 ms (14.3%), Space: 43.4 MB (76.96%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.java
@@ -0,0 +1,18 @@
+class Solution {
+
+    public void sortColors(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            // Last i elements are already in place
+            for (int j = 0; j < n - i - 1; j++) {
+                // Swap if the element found is greater than the next element
+                if (nums[j] > nums[j + 1]) {
+                    // swap temp and nums[j]
+                    int temp = nums[j];
+                    nums[j] = nums[j + 1];
+                    nums[j + 1] = temp;
+                }
+            }
+        }
+    }
+}
